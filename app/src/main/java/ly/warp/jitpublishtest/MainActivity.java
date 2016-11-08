1e7c8d2e328e2ca27478e206ae853c9547287e2d
Alexander Efimov <alexander.efimov@exairo.com>
2016-11-08 13:04:58 +0300

    first commit

---
+++ app/src/main/java/ly/warp/jitpublishtest/MainActivity.java
@@ -0,0 +1,15 @@
+package ly.warp.jitpublishtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ly.warp.jitpublishtestlib.Utils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ jitpublishtestlib/src/main/java/ly/warp/jitpublishtestlib/Utils.java
@@ -0,0 +1,46 @@
+package ly.warp.jitpublishtestlib;
+
+import android.os.Build;
+
+public class Utils {
+
+    public static boolean hasHoneyComb() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+    }
+
+    public static boolean hasIceCreamSandwich() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+    }
+
+    public static boolean hasIceCreamSandwichMR1() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1;
+    }
+
+    public static boolean hasJellyBean() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+    }
+
+    public static boolean hasJellyBeanMR2() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2;
+    }
+
+    public static boolean hasKitKat() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+    }
+
+    public static boolean hasLollipop() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+
+    public static boolean hasLollipopMR1() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1;
+    }
+
+    public static boolean hasMarshmallow() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    }
+
+    public static boolean hasNougat() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N;
+    }
+}
